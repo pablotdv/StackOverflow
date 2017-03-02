ae57bc87a764fe786adf20596c423bc4cd20e3b5
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2017-03-02 14:23:34 -0300

    Exemplo SO

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_endereco = new System.Windows.Forms.TextBox();
+            this.txt_ano = new System.Windows.Forms.TextBox();
+            this.txt_tele = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(12, 12);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 0;
+            // 
+            // txt_endereco
+            // 
+            this.txt_endereco.Location = new System.Drawing.Point(12, 38);
+            this.txt_endereco.Name = "txt_endereco";
+            this.txt_endereco.Size = new System.Drawing.Size(100, 20);
+            this.txt_endereco.TabIndex = 1;
+            // 
+            // txt_ano
+            // 
+            this.txt_ano.Location = new System.Drawing.Point(12, 64);
+            this.txt_ano.Name = "txt_ano";
+            this.txt_ano.Size = new System.Drawing.Size(100, 20);
+            this.txt_ano.TabIndex = 2;
+            // 
+            // txt_tele
+            // 
+            this.txt_tele.Location = new System.Drawing.Point(12, 90);
+            this.txt_tele.Name = "txt_tele";
+            this.txt_tele.Size = new System.Drawing.Size(100, 20);
+            this.txt_tele.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(191, 241);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 430);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_tele);
+            this.Controls.Add(this.txt_ano);
+            this.Controls.Add(this.txt_endereco);
+            this.Controls.Add(this.txt_nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_endereco;
+        private System.Windows.Forms.TextBox txt_ano;
+        private System.Windows.Forms.TextBox txt_tele;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        List<Pessoa> list = new List<Pessoa>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Add(new Pessoa(txt_ano.Text, txt_nome.Text, txt_endereco.Text, txt_tele.Text));
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = list;
+        }
+    }
+
+    public class Pessoa
+    {
+        private string nome;
+        private string endereço;
+        private string ano_nascimento;
+        private string telefone;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            private set
+            {
+                nome = value;
+            }
+        }
+
+        public string Endereço
+        {
+            get
+            {
+                return endereço;
+            }
+
+            private set
+            {
+                endereço = value;
+            }
+        }
+
+        public string Ano_nascimento
+        {
+            get
+            {
+                return ano_nascimento;
+            }
+
+            private set
+            {
+                ano_nascimento = value;
+            }
+        }
+
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+
+            private set
+            {
+                telefone = value;
+            }
+        }
+
+        public Pessoa(string nome, string endereço, string ano_nascimento, string telefone)
+        {
+            this.nome = nome;
+
+            this.endereço = endereço;
+
+            this.ano_nascimento = ano_nascimento;
+
+            this.telefone = telefone;
+
+        }
+    }
+}
